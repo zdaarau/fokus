d7e63560b263e03aca915ecd9b5bb0947a5a6d40
Salim B <git@salim.space>
2024-01-09 00:16:00 +0100

    add `combos_proposals()` and tweak doc

--- tests/testthat/test-fokus.R
+++ tests/testthat/test-fokus.R
@@ -63,6 +63,15 @@ test_that("Predicate functions return correct default values", {
                    6L)
   expect_identical(n_proposal_main_motives(),
                    13L)
+  expect_identical(combos_proposals(),
+                   list(list(lvl = "cantonal",
+                             proposal_nr = 1L),
+                        list(lvl = "federal",
+                             proposal_nr = 1L),
+                        list(lvl = "federal",
+                             proposal_nr = 2L),
+                        list(lvl = "federal",
+                             proposal_nr = 3L)))
   expect_identical(combos_proposal_arguments(),
                    list(list(lvl = "cantonal",
                              proposal_nr = 1L,
